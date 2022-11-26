0d8ee4a62c4a1c012667e952670fb8741e8c127a
enanesh <edna.ganesha.gr@gmail.com>
2022-11-26 04:13:14 -0800

    Changes to README template

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown =require('./utils/generateMarkdown.js');
 
 
 // TODO: Create an array of questions for user input
@@ -44,46 +45,72 @@ const questions = inquirer.prompt([
             type: "list",
             message: "License:",
             name: "license",
-            choices: ['Academic Free License v3.0', 'perro'],
+            choices: ['Apache 2.0 License','Boost Software License 1.0'],
             validate: (value) => { if (value) { return true } else { return "Input a value to continue" } }
 
-         },
-         {
+    },
+    {
         type: "input",
-        message: "List your collaborators, if any, with links to their GitHub profiles.",
-        name: "questions",
+        message: "If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so",
+        name:"contributions",
         validate: (value) => { if (value) { return true } else { return "Input a value to continue" } }
 
-         },
+    },
+    {
+        type: "input",
+        message: "Go the extra mile and write tests for your application. Then provide examples on how to run them here. ",
+        name: "test",
+        validate: (value) => { if (value) { return true } else { return "Input a value to continue" } }
+
+    },
+    {
+        type: "input",
+        message: "Please, provide your GitHub username(e.g JohnYoung) : ",
+        name: "gitHub",
+        validate: (value) => { if (value) { return true } else { return "Input a value to continue" } }
+
+    },
+    
+    {
+        type: "input",
+        message: "Please, provide your email(e.g student@gmail.com)",
+        name: "email",
+        validate: (value) => { if (value) { return true } else { return "Input a value to continue" } }
+
+    },
           
-        
-        
-        
+         
         
       
  ])
-    .then((answers) => {
+//     .then((answers) => {
+//         title,
+            
 
-        answers.confirm === answers.password
-            ? console.log("Success!")
-            : console.log("You forgot your password already?!");
+       
+
+//         // TODO: Create a function to write README file
+//         function writeToFile(fileName, data) {
+
+//             fs.writeFile(`./${fileName.toLowerCase().split().joint(" ")}.md`, data, (err) => {
+//                 if (err) {
+//                     console.log(err)
+//                 }
+//                 console.log("Your README has been generated");
+//             })
+//         };
+
+//         // TODO: Create a function to initialize app
+        
+            
+        
+//     });
 
 
-        // TODO: Create a function to write README file
-        function writeToFile(fileName, data) {
+// // function init() {
 
-            fs.writeFile(`./${fileName.toLowerCase().split().joint(" ")}.md`, data, (err) => {
-                if (err) {
-                    console.log(err)
-                }
-                console.log("Your README has been generated");
-            })
-        };
 
-        // TODO: Create a function to initialize app
-        // function init() {
-        //     inquirer.prompt(questions)
-    });
+//  }
 // Function call to initialize app
 // init();
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,57 +1,89 @@
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { 
-  console.log("Hola")
-}
 
+function renderLicenseBadge(license) {
+  let badge = " ";
+  if (license === "Apache 2.0 License") {
+    badge = "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]"
+  } else if (license === "Boost Software License 1.0") {
+    badge = "![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)"
+  } else {
+    badge = " "
+  }
+  return badge;
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
+
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+
+
 ##Description
 
-
+#${data.description}
 
 
 ##Table of Contents
 
-[Installation](#installation)
-[Usage](#usage)
-[Credits](#credits)
-[License](#license)  
+### *[Installation](#installation)
+### *[Usage](#usage)
+### *[Credits](#credits)
+### *[License](#license)  
 
 
 
 ##Installation
-
+###You must follow the steps for this app to function:
+### ${data.installation}
 
 
 
 ##Usage
 
+###${data.usage}
+
 
 
 ##Credits
 
-
+###${data.credits}
 
 
 ##License
 
+##${data.license}
+
 
 ##Contributing
 
+###${data.contributions}
+
 ##Tests
 
+### To test this application run the following commands in your terminal:
+### ${data.test}
+
+
+
 ##Questions
+### If you have any questions, you may be able to contact me at either
+### GitHub: https://github.com/${data.gitHub}
+### or
+### Email: ${data.email}
 
   
   `;
@@ -59,4 +91,4 @@ function generateMarkdown(data) {
 
 
 
-module.exports = generateMarkdown();
+module.exports = generateMarkdown;
